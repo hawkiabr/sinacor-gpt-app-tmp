6ed7c5faa267460705cee196be71f9a356a38c29
André Marques de Araújo <andre@hawkia.com.br>
2023-12-21 11:34:02 -0300

    Primeira versão do app.

---
+++ app/backend/static/assets/NoPage-cbf595ab.js
@@ -0,0 +1,2 @@
+import{bo as o}from"./vendor-541a2ba1.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-cbf595ab.js.map
